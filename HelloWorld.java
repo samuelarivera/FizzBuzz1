465499283e3f8f2cc8a2a1fb6547b32c73bc96b8
Samuel Rivera <samuel.rivera@mrjaffesclass.com>
2023-10-16 11:49:13 -0700

    fizzbuzz done using repeated division loops

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,9 +2,36 @@ public class HelloWorld
 {
     public static void main(String[] args) 
     {
-            int i;   
-            float f = 2.5f;
-            boolean flag = true;
+        boolean fizzcheck = false;
+        boolean buzzcheck = false;
+        for(float i =0; i<=100; i++){
+            fizzcheck = false;
+            buzzcheck = false;
+            for(int xthree = 33; xthree > 0; xthree-- ){
+             if(i/ (xthree * 3) == 1){
+                 fizzcheck = true;
+             }
+            }
+            for(int xfive = 20; xfive > 0; xfive--){
+                if(i/(xfive * 5) == 1){
+                    buzzcheck = true;
+                }
+            }
+            if (fizzcheck == false && buzzcheck == false){
+               System.out.println(i); 
+            }else{
+                if(fizzcheck == true && buzzcheck == true){
+                    System.out.println("fizzbuzz");
+                }else{
+                    if( fizzcheck == true){
+                        System.out.println("fizz");
+                    }else{
+                        if(buzzcheck == true){
+                            System.out.println("buzz");
+                        }   
+                    }
+                }
+            }
+        }
     }
 }
-
